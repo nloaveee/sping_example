94e616a96b91e9237b02981f478a011f7bdf0a28
hyeonheee <nloavee@gmail.com>
2024-06-13 18:42:42 +0900

    lesson02까지

--- src/main/java/com/example/lesson02/Lesson02Ex01RestController.java
+++ src/main/java/com/example/lesson02/Lesson02Ex01RestController.java
@@ -2,18 +2,26 @@ package com.example.lesson02;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.lesson02.bo.UsedGoodsBO;
 import com.example.lesson02.domain.UsedGoods;
 
 @RestController // @Controller + @ResponseBody (JSON)
 public class Lesson02Ex01RestController {
 
-	// http://localhost/lesson01/ex01
-	@RequestMapping("/lesson01/ex01")
-	public List<UsedGoods> ex01() {
+	@Autowired
+	private UsedGoodsBO usedGoodsBO; // Dependency inject
 
+	// http://localhost/lesson02/ex01
+	@RequestMapping("/lesson02/ex01")
+	public List<UsedGoods> ex01() {
+//		List<UsedGoods> usedGoodsList = usedGoodsBO.getUsedGoodsList();
+//		return usedGoodsList; // list => JSON String
+
+		return usedGoodsBO.getUsedGoodsList();
 	}
 
 }
--- src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -2,17 +2,27 @@ package com.example.lesson02.bo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson02.domain.UsedGoods;
+import com.example.lesson02.mapper.UsedGoodsMapper;
 
 @Service // spring bean
 public class UsedGoodsBO {
 
+	// Spring bean을 Autowired를 통해서 가져온다.
+	@Autowired // Dependency Inject(DI): 의존성 주입
+	private UsedGoodsMapper usedGoodsMapper;
+
 	// input: x (controller로부터 들어온 요청)
 	// output: (Respository한테 받은것을 controller한테 준다) List<UsedGoods>
 	public List<UsedGoods> getUsedGoodsList() {
+//		List<UsedGoods> usedGoodsList = usedGoodsMapper.selectUsedGoodsList();
+//		return usedGoodsList;
 
+		// 위의 코드와 동일
+		return usedGoodsMapper.selectUsedGoodsList(); // controller에게 반환
 	}
 
 }
